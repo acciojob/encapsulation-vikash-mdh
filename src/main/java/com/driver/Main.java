e58d8ecf0b2adaeae517d47ce7fd950cd39275ea
unknown <vikashkumar555550@gmail.com>
2024-11-04 16:37:15 +0530

    write the logic part

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,18 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+
+        // Task 3: Create an object of class RWOnly
+        RWOnly obj = new RWOnly();
+
+        // Task 4: Try setting a value to name by directly accessing it using obj and also try printing it
+        // Uncommenting the following lines will result in an error
+        // obj.name = "John";  // Error: name has private access in RWOnly
+        // System.out.println(obj.name);  // Error: name has private access in RWOnly
+
+        // Task 6: Set value using the setter and get value using the getter
+        obj.setName("John");
+        System.out.println(obj.getName());  // Output: John
+    }
 }
--- src/main/java/com/driver/RWOnly.java
+++ src/main/java/com/driver/RWOnly.java
@@ -1,5 +1,15 @@
 package com.driver;
 
 public class RWOnly {
+    private String name;
+
+    // Task 5: Implement getter and setter functions for name
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
     
 }
